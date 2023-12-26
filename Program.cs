7837eeec79f6e197d3c779267aeafa5df6d4cdeb
Dragonan <john.spasov@gmail.com>
2023-12-26 19:03:53 +0200

    Added QoL functionalities

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace AOC2023
 {
@@ -6,7 +7,18 @@ namespace AOC2023
 	{
 		public static void Main()
 		{
+			var timer = new Stopwatch();
+			timer.Start();
+
+
 			Day1.Solve();
+			
+			
+			timer.Stop();
+			if (timer.ElapsedMilliseconds > 999)
+				Console.WriteLine($"Time: {timer.Elapsed.TotalSeconds}s");
+			else
+				Console.WriteLine($"Time: {timer.ElapsedMilliseconds}ms");;
 		}
 	}
 }
