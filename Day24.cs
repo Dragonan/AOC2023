537ef57294ee693e077c5e6c65add425ea5a7c65
Dragonan <john.spasov@gmail.com>
2024-01-07 17:32:13 +0200

    Day 24 Part 1 complete

--- Day24.cs
+++ Day24.cs
@@ -2,6 +2,18 @@ using System.Numerics;
 
 namespace AOC2023
 {
+    public struct PointL
+    {
+        public PointL(long x, long y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public long X { get; }
+        public long Y { get; }
+    }
+
     public class Hailstone
     {
         public Hailstone(Vector<long> coords, Vector<long> velocity)
@@ -9,6 +21,7 @@ namespace AOC2023
             Coords = coords;
             Velocity = velocity;
             EndCoords = Vector<long>.Zero;
+            StartCoords = Vector<long>.Zero;
         }
 
         public Vector<long> Coords { get; }
@@ -19,11 +32,16 @@ namespace AOC2023
         public long XChange => Velocity[0];
         public long YChange => Velocity[1];
         public long ZChange => Velocity[2];
+        public Vector<long> StartCoords { get; set; }
+        public long XStart => StartCoords[0];
+        public long YStart => StartCoords[1];
+        public long ZStart => StartCoords[2];
         public Vector<long> EndCoords { get; set; }
         public long XEnd => EndCoords[0];
         public long YEnd => EndCoords[1];
         public long ZEnd => EndCoords[2];
-        public bool EndCalculated { get; set; }
+        public bool SegmentCalculated { get; set; }
+        public bool IsOutOfBounds { get; set; }
     }
 
     public static class Day24
@@ -39,61 +57,100 @@ namespace AOC2023
 
         static int GetCollidedHailstonesForPart1()
         {
-            var min = 7;
-            var max = 27;
+            var min = 200000000000000;
+            var max = 400000000000000;
+            var simplifier = 1000000;
+            //var min = 7;
+            //var max = 27;
             var count = 0;
             
             for (int a = 0; a < Hailstones.Count-1; a++)
             {
                 var aSegment = Hailstones[a];
-                FindLimits(aSegment, min, max);
+                if (aSegment.IsOutOfBounds || (aSegment.IsOutOfBounds = IsHailstoneOutOfBounds(aSegment,min,max)))
+                    continue;
+
+                FindSegment(aSegment, min, max);
+                if (aSegment.IsOutOfBounds)
+                    continue;
+
                 for (int b = a+1; b < Hailstones.Count; b++)
                 {
                     var bSegment = Hailstones[b];
-                    FindLimits(bSegment, min, max);
-                    
-                    
-                    var dxA = aSegment.XEnd - aSegment.X;
-                    var dxB = bSegment.XEnd - bSegment.X;
-                    var dyA = aSegment.YEnd - aSegment.Y;
-                    var dyB = bSegment.YEnd - bSegment.Y;
-                    var p0 = dyB*(bSegment.XEnd - aSegment.X) - dxB*(bSegment.YEnd - aSegment.Y);
-                    var p1 = dyB*(bSegment.XEnd - aSegment.XEnd) - dxB*(bSegment.YEnd - aSegment.YEnd);
-                    var p2 = dyA*(aSegment.XEnd - bSegment.X) - dxA*(aSegment.YEnd - bSegment.Y);
-                    var p3 = dyA*(aSegment.XEnd - bSegment.XEnd) - dxA*(aSegment.YEnd - bSegment.YEnd);
+                    if (bSegment.IsOutOfBounds || (bSegment.IsOutOfBounds = IsHailstoneOutOfBounds(bSegment,min,max)))
+                        continue;
 
-                    if (p0 * p1 <= 0 && p2 * p3 <= 0)
+                    FindSegment(bSegment, min, max);
+                    if (bSegment.IsOutOfBounds)
+                        continue;
+
+                    if (ExtraMath.DoIntersect(new PointL(aSegment.XStart/simplifier, aSegment.YStart/simplifier), new PointL(aSegment.XEnd/simplifier, aSegment.YEnd/simplifier),
+                                              new PointL(bSegment.XStart/simplifier, bSegment.YStart/simplifier), new PointL(bSegment.XEnd/simplifier, bSegment.YEnd/simplifier)))
+                    {
                         count++;
+                        //Console.WriteLine("Lines {0} and {1} intersect", (a+1).ConvertToLetterName(), (b+1).ConvertToLetterName());
+                    }
+                    //else
+                        //Console.WriteLine("Lines {0} and {1} do not intersect", (a+1).ConvertToLetterName(), (b+1).ConvertToLetterName());
                 }
             }
 
             return count;
         }
 
-        static void FindLimits(Hailstone hailstone, long min, long max, bool includeZ = false)
+        static bool IsHailstoneOutOfBounds(Hailstone hailstone, long min, long max)
         {
-            if (hailstone.EndCalculated)
+            return (hailstone.X < min && hailstone.XChange <= 0) || 
+                    (hailstone.X > max && hailstone.XChange >= 0) ||
+                    (hailstone.Y < min && hailstone.YChange <= 0) || 
+                    (hailstone.Y > max && hailstone.YChange >= 0);
+        }
+
+        static void FindSegment(Hailstone hailstone, long min, long max, bool includeZ = false)
+        {
+            if (hailstone.IsOutOfBounds || hailstone.SegmentCalculated)
                 return;
 
-            var minX = hailstone.XChange >= 0 ? hailstone.X : min;
-            var maxX = hailstone.XChange >= 0 ? max : hailstone.X;
-            var minY = hailstone.YChange >= 0 ? hailstone.Y : min;
-            var maxY = hailstone.YChange >= 0 ? max : hailstone.Y;
-            var minZ = hailstone.ZChange >= 0 ? hailstone.Z : min;
-            var maxZ = hailstone.ZChange >= 0 ? max : hailstone.Z;
-            var xTime = (long)Math.Ceiling((maxX - minX)/(double)hailstone.XChange);
-            var yTime = (long)Math.Ceiling((maxY - minY)/(double)hailstone.ZChange);
-            var zTime = (long)Math.Ceiling((maxZ - minZ)/(double)hailstone.ZChange);
-            var time = Math.Min(xTime,yTime);
+            long xTimeToStart = 0, yTimeToStart = 0, zTimeToStart = 0;
+            if (hailstone.X < min || hailstone.X > max)
+                xTimeToStart = (long)Math.Abs(Math.Ceiling((Math.Max(min, hailstone.X) - Math.Min(hailstone.X, max))/(double)hailstone.XChange));
+            if (hailstone.Y < min || hailstone.Y > max)
+                yTimeToStart = (long)Math.Abs(Math.Ceiling((Math.Max(min, hailstone.Y) - Math.Min(hailstone.Y, max))/(double)hailstone.YChange));
+            if (hailstone.Z < min || hailstone.Z > max)
+                zTimeToStart = (long)Math.Abs(Math.Ceiling((Math.Max(min, hailstone.Z) - Math.Min(hailstone.Z, max))/(double)hailstone.ZChange));
+            var timeToStart = Math.Max(xTimeToStart,yTimeToStart);
             if (includeZ)
-                time = Math.Min(time,zTime);
-            
-            var xEnd = hailstone.X + hailstone.XChange*time;
-            var yEnd = hailstone.Y + hailstone.YChange*time;
-            var zEnd = hailstone.Z + hailstone.ZChange*time;
+                timeToStart = Math.Max(timeToStart,zTimeToStart);
+            var xStart = hailstone.X + hailstone.XChange*timeToStart;
+            var yStart = hailstone.Y + hailstone.YChange*timeToStart;
+            var zStart = hailstone.Z + hailstone.ZChange*timeToStart;
 
+            if (xStart < min || (max-hailstone.XChange) < xStart ||
+                yStart < min || max-hailstone.YChange < yStart ||
+                (includeZ && (zStart < min || max-hailstone.ZChange < zStart)))
+            {
+                hailstone.IsOutOfBounds = true;
+                return;
+            }
+
+            var xLength = hailstone.XChange >= 0 ? max - xStart : xStart - min;
+            var yLength = hailstone.YChange >= 0 ? max - yStart : yStart - min;
+            var zLength = hailstone.ZChange >= 0 ? max - zStart : zStart - min;
+            var xTimeToEnd = (long)Math.Abs(Math.Ceiling(xLength/(double)hailstone.XChange));
+            var yTimeToEnd = (long)Math.Abs(Math.Ceiling(yLength/(double)hailstone.YChange));
+            var zTimeToEnd = (long)Math.Abs(Math.Ceiling(zLength/(double)hailstone.ZChange));
+            var timeToEnd = Math.Min(xTimeToEnd,yTimeToEnd);
+            if (includeZ)
+                timeToEnd = Math.Min(timeToEnd,zTimeToEnd);
+            var xEnd = xStart + hailstone.XChange*timeToEnd;
+            var yEnd = yStart + hailstone.YChange*timeToEnd;
+            var zEnd = zStart + hailstone.ZChange*timeToEnd;
+
+
+
+            hailstone.StartCoords = new Vector<long>(new [] { xStart, yStart, zStart, 0L });
             hailstone.EndCoords = new Vector<long>(new [] { xEnd, yEnd, zEnd, 0L });
-            hailstone.EndCalculated = true;
+            hailstone.SegmentCalculated = true;
         }
 
         static void ParseHailstones(string[] rows)
--- ExtraMath.cs
+++ ExtraMath.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace AOC2023
 {
     public static class ExtraMath
@@ -41,5 +43,64 @@ namespace AOC2023
             var n = max/2;
             return n*(n+1);
         }
+
+        // Given three collinear points p, q, r, the function checks if 
+        // point q lies on line segment 'pr' 
+        public static bool OnSegment(PointL p, PointL q, PointL r) 
+        { 
+            if (q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) && 
+                q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y)) 
+            return true; 
+        
+            return false; 
+        } 
+  
+        // To find orientation of ordered triplet (p, q, r). 
+        // The function returns following values 
+        // 0 --> p, q and r are collinear 
+        // 1 --> Clockwise 
+        // 2 --> Counterclockwise 
+        public static long Orientation(PointL p, PointL q, PointL r) 
+        { 
+            // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
+            // for details of below formula. 
+            var val = (q.Y - p.Y) * (r.X - q.X) - 
+                    (q.X - p.X) * (r.Y - q.Y); 
+        
+            if (val == 0) return 0; // collinear 
+        
+            return (val > 0)? 1: 2; // clock or counterclock wise 
+        } 
+  
+        // The main function that returns true if line segment 'p1q1' 
+        // and 'p2q2' intersect. 
+        public static bool DoIntersect(PointL p1, PointL q1, PointL p2, PointL q2) 
+        { 
+            // Find the four orientations needed for general and 
+            // special cases 
+            var o1 = Orientation(p1, q1, p2); 
+            var o2 = Orientation(p1, q1, q2); 
+            var o3 = Orientation(p2, q2, p1); 
+            var o4 = Orientation(p2, q2, q1); 
+        
+            // General case 
+            if (o1 != o2 && o3 != o4) 
+                return true; 
+        
+            // Special Cases 
+            // p1, q1 and p2 are collinear and p2 lies on segment p1q1 
+            if (o1 == 0 && OnSegment(p1, p2, q1)) return true; 
+        
+            // p1, q1 and q2 are collinear and q2 lies on segment p1q1 
+            if (o2 == 0 && OnSegment(p1, q2, q1)) return true; 
+        
+            // p2, q2 and p1 are collinear and p1 lies on segment p2q2 
+            if (o3 == 0 && OnSegment(p2, p1, q2)) return true; 
+        
+            // p2, q2 and q1 are collinear and q1 lies on segment p2q2 
+            if (o4 == 0 && OnSegment(p2, q1, q2)) return true; 
+        
+            return false; // Doesn't fall in any of the above cases 
+        } 
     }
 }
